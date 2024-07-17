8875571cae0f2a21baf66f06e402ddb8e2c62d07
Manthan Ankolekar <manthan.ank46@gmail.com>
2024-07-18 00:02:38 +0530

    chore: Update API URL in environment files

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: window.location.hostname === 'localhost' ? 'http://localhost:3000/api' : 'https://url-shortener-app-k27w.onrender.com/api'
+    apiUrl: window.location.hostname === 'localhost' ? 'http://localhost:3000/api' : 'https://url-shortener-app-nrnh.vercel.app/api'
 };
