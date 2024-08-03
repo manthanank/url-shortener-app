ccf03161bae35edb15a5709a55c8aa5ab89c4b4f
Manthan Ankolekar <manthan.ank46@gmail.com>
2024-08-03 11:46:25 +0530

    refactor: Update meta tags in index.html for SEO optimization
    The code changes include:
    - Added additional keywords to the meta tags in index.html to improve search engine optimization (SEO)
    - Updated the author name and added a robots meta tag for better indexing and crawling by search engines.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { Meta } from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,4 +10,22 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'url-shortener-app';
+
+  meta = inject(Meta);
+
+  constructor() {
+    this.meta.addTags([
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'description', content: 'Easily shorten your long URLs with our simple and efficient URL shortener app.' },
+      { name: 'keywords', content: 'URL shortener, link shortener, shorten URL, link management, angular, nodejs. express, mongodb' },
+      { name: 'author', content: 'Manthan Ankolekar' },
+      { name: 'robots', content: 'index, follow' },
+      { rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' },
+      { rel: 'canonical', href: 'https://shortener-url-app.vercel.app/' },
+      { property: 'og:title', content: 'Url Shortener App' },
+      { property: 'og:description', content: 'Easily shorten your long URLs with our simple and efficient URL shortener app.' },
+      { property: 'og:image', content: 'https://shortener-url-app.vercel.app/image.jpg' },
+      { property: 'og:url', content: 'https://shortener-url-app.vercel.app/' }
+    ]);
+  }
 }
